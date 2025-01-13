9bfa4e95a073bb19b6508746e734061840a43fdd
Suraj370 <surajpanda2077@gmail.com>
2025-01-13 18:58:25 +0530

    fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/kanban-board" element={<Index />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
